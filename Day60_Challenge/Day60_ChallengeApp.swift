0bdaf11cf2d6ed8b2007caff5fd5ed991c038adf
xHadi3 <hadiessam9@gmail.com>
2025-02-24 23:59:04 +0300

    Initial Commit

---
+++ Day60_Challenge/Day60_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60_ChallengeApp.swift
+//  Day60_Challenge
+//
+//  Created by Hadi Al zayer on 25/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Day60_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
