f73cc2916c7abe6316df0b00192bc747240afdaf
xHadi3 <hadiessam9@gmail.com>
2025-03-19 22:57:47 +0300

    making the model and the user services

---
+++ Day60_Challenge/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Day60_Challenge
+//
+//  Created by Hadi Al zayer on 19/09/1446 AH.
+//
+
+import Foundation
---
+++ Day60_Challenge/user.swift
@@ -0,0 +1,8 @@
+//
+//  user.swift
+//  Day60_Challenge
+//
+//  Created by Hadi Al zayer on 19/09/1446 AH.
+//
+
+import Foundation
